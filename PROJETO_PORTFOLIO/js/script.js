847e8fb7b85304f334d6f61e6b68f5c2c5877c61
dErnandoV7 <d.ernandov7@gmail.com>
2023-09-20 21:19:12 -0300

    up

--- PROJETO_PORTFOLIO/js/script.js
+++ PROJETO_PORTFOLIO/js/script.js
@@ -1,7 +1,8 @@
 function themeMode() {
   let mode = document.querySelector(".themeModeImg")
-  if (mode.src == "http://localhost:7700/PROJETO_PORTFOLIO/img/light.svg") {
-    mode.src = "http://localhost:7700/PROJETO_PORTFOLIO/img/dark.svg"
+  if (mode.classList.contains("on")) {
+    mode.classList.remove("on")
+    mode.src = "img/light.svg"
     document.querySelector("header").style.background = "#160828"
     document.querySelector("main").style.background = "#160828"
     document.querySelector("footer").style.background = "#160828"
@@ -9,12 +10,16 @@ function themeMode() {
     document.querySelector(".description h1").style.color = "#ddd"
     document.querySelector(".description h2").style.color = "#ddd"
     document.querySelector(".projetos h2").style.color = "#ddd"
-    document.querySelector(".linkedin img").src = "http://localhost:7700/PROJETO_PORTFOLIO/img/linkedinDark.svg"
-    document.querySelector(".code img").src = "http://localhost:7700/PROJETO_PORTFOLIO/img/code-slashDark.svg"
-    document.querySelector(".github img").src = "http://localhost:7700/PROJETO_PORTFOLIO/img/githubDark.svg"
-    document.querySelector(".listSideBar img").src = "http://localhost:7700/PROJETO_PORTFOLIO/img/listDark.svg"
+    document.querySelector(".linkedin img").src = "img/linkedinDark.svg"
+    document.querySelector(".code img").src = "img/code-slashDark.svg"
+    document.querySelector(".github img").src = "img/githubDark.svg"
+    document.querySelector(".listSideBar img").src = "img/listDark.svg"
+    document.querySelectorAll(".icon-l")[0].classList.add("filterDarkIcon")
+    document.querySelectorAll(".icon-l")[1].classList.add("filterDarkIcon")
+    document.querySelectorAll(".icon-l")[2].classList.add("filterDarkIcon")
   } else {
-    mode.src = "http://localhost:7700/PROJETO_PORTFOLIO/img/light.svg"
+    mode.classList.add("on")
+    mode.src = "img/dark.svg"
     document.querySelector("header").style.background = "#fff"
     document.querySelector("main").style.background = "#fff"
     document.querySelector("footer").style.background = "#fff"
@@ -22,10 +27,12 @@ function themeMode() {
     document.querySelector(".description h1").style.color = "#000"
     document.querySelector(".description h2").style.color = "#000"
     document.querySelector(".projetos h2").style.color = "#000"
-    document.querySelector(".linkedin img").src = "http://localhost:7700/PROJETO_PORTFOLIO/img/linkedinLight.svg"
-    document.querySelector(".code img").src = "http://localhost:7700/PROJETO_PORTFOLIO/img/code-slashLight.svg"
-    document.querySelector(".github img").src = "http://localhost:7700/PROJETO_PORTFOLIO/img/githubLight.svg"
-    document.querySelector(".listSideBar img").src = "http://localhost:7700/PROJETO_PORTFOLIO/img/listLight.svg"
-
+    document.querySelector(".linkedin img").src = "img/linkedinLight.svg"
+    document.querySelector(".code img").src = "img/code-slashLight.svg"
+    document.querySelector(".github img").src = "img/githubLight.svg"
+    document.querySelector(".listSideBar img").src = "img/listLight.svg"
+    document.querySelectorAll(".icon-l")[0].classList.remove("filterDarkIcon")
+    document.querySelectorAll(".icon-l")[1].classList.remove("filterDarkIcon")
+    document.querySelectorAll(".icon-l")[2].classList.remove("filterDarkIcon")
   }
 }
