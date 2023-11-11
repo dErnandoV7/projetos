8ff73475f039a633fb848e104b0899492942d8d8
dErnandoV7 <d.ernandov7@gmail.com>
2023-11-11 14:29:52 -0300

    up

---
+++ LAZY_LOAD/script.js
@@ -0,0 +1,15 @@
+const allImg = document.querySelectorAll(".image-container img")
+
+const observerImg = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) return
+        const img = entry.target
+        img.src = img.src.replace("&w=10&", "&w=1000&")
+        observer.unobserve(img);
+    })
+}, {})
+
+allImg.forEach(img => {
+    observerImg.observe(img)
+})
+
